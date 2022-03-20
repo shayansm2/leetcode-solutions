74fea0ef7893499fcfc13fdbd45814aca8e60541
shayan shafieemoghadam <s.shafieemoghadam>
2022-03-20 14:56:10 +0330

    add a file inorder to create TreeNode from array for binary tree questions

---
+++ src/lib/binaryTree.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
+
+
+def deserialize(string):
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
