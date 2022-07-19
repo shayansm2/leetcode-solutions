4ac6771937c75b5984089fade75bde1febb13c84
shayansm <s.shafieemoghadam@digikala.com>
2022-07-20 00:52:22 +0430

    solved one easy problem

---
+++ src/easy/PascalTriangle.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascals = [[1 for i in range(numRows)]]
+
+        for i in range(1, numRows):
+            row = [1]
+            
+            for j in range(1, numRows - i):
+                row.append(row[j-1] + pascals[i-1][j])
+            
+            pascals.append(row)
+        
+        output = []
+        
+        for i in range(1, numRows+1):
+            row = []
+            for j in range(i):
+                # print(j, i-j-1)
+                row.append(pascals[j][i-j-1])
+            output.append(row)
+        
+        return output
