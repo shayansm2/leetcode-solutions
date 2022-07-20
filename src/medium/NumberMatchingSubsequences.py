7481c026152bb43f99678f287295095aabf77ad9
shayansm <s.shafieemoghadam@digikala.com>
2022-07-20 18:54:51 +0430

    solved one medium problem

---
+++ src/medium/NumberMatchingSubsequences.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        self.letter_index_map = dict()
+
+        for index, letter in enumerate(s):
+            try:
+                self.letter_index_map[letter].append(index)
+            except:
+                self.letter_index_map[letter] = [index]
+
+        # print(self.letter_index_map)
+
+        counter = 0
+        for word in words:
+            if self.isSubsequent(word):
+                counter += 1
+                # print(word)
+
+        return counter
+
+    def isSubsequent(self, word) -> bool:
+        max_index = -1
+
+        for letter in word:
+            try:
+                indices = self.letter_index_map[letter]
+            except:
+                return False
+
+            if max_index >= indices[-1]:
+                # print(max_index, indices) 
+                return False
+
+            for index in indices:
+                if index > max_index:
+                    max_index = index
+                    break
+
+        return True
