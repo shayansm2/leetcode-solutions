459633781f35dcc00748a77899d00e1051754586
shayansm <shayansm09@gmail.com>
2023-03-23 10:03:25 +0330

    better

--- src/easy/IsomorphicStrings.py
+++ src/easy/IsomorphicStrings.py
@@ -2,12 +2,12 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         mapper = {}
         reverse_mapper = {}
-        
+
         if len(s) != len(t):
-            return false
-        
+            return False
+
         length = len(s)
-        
+
         for index in range(length):
             if mapper.__contains__(s[index]):
                 if mapper[s[index]] != t[index]:
@@ -15,8 +15,8 @@ class Solution:
             else:
                 if reverse_mapper.__contains__(t[index]):
                     return False
-                
+
                 mapper[s[index]] = t[index]
                 reverse_mapper[t[index]] = s[index]
-        
+
         return True
