39a92df03fe8cef27bc8509434417817405c0921
shayansm <shayansm09@gmail.com>
2023-03-27 12:18:07 +0330

    some improvements, but not good enough for solving the problem

--- src/unsolved/LongestCycleGraph.py
+++ src/unsolved/LongestCycleGraph.py
@@ -2,17 +2,24 @@ class Solution:
     def longestCycle(self, edges: list[int]) -> int:
         longest_cycle = -1
         visited = [False for _ in range(len(edges))]
+
         for node in range(len(edges)):
             if visited[node]:
+                print('badi')
                 continue
 
-            queue = [node]
-            turn = 0
-            turns = {}
+            print('new graph starting with', node)
 
-            while queue:
+            next_node = node
+            turn = 0
+            turns = dict()
+
+            while next_node is not None:
                 turn += 1
-                current_node = queue.pop(0)
+                current_node, next_node = next_node, None
+
+                print(sum(visited), current_node, turn)
+
                 visited[current_node] = True
 
                 turns[current_node] = turn
@@ -20,12 +27,23 @@ class Solution:
                 next_node = edges[current_node]
 
                 if next_node == -1:
-                    continue
+                    break
 
                 if next_node in turns:
-                    longest_cycle=max(longest_cycle, turn + 1 - turns[next_node])
-                    continue
+                    longest_cycle = max(longest_cycle, turn + 1 - turns[next_node])
+                    break
 
-                queue.append(next_node)
+                if visited[next_node]:
+                    break
 
         return longest_cycle
+
+
+if __name__ == '__main__':
+    in1 = [3, 3, 4, 2, 3]
+    in2 = [2, -1, 3, 1]
+    in3 = [3, 4, 0, 2, -1, 2]
+    in4 = [-1, 4, -1, 2, 0, 4]
+    in5 = list(map(int, input()[1:-1].split(',')))
+    in6 = [1, 2, 3, 4, 5, 6, 7, 8, 1, 1, 1, 1, 2, 3]
+    print(Solution().longestCycle(in5))
