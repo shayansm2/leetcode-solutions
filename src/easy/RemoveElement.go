234470421da0463d1eb89f6462f0a2a8a639d94a
shayansm <shayansm09@gmail.com>
2023-05-13 17:27:14 +0330

    a bit better

--- src/easy/RemoveElement.go
+++ src/easy/RemoveElement.go
@@ -2,15 +2,13 @@ package main
 
 func removeElement(nums []int, val int) int {
 	index, replaceIndex := 0, len(nums)-1
-	var counter int
 	for index <= replaceIndex {
 		if nums[index] == val {
 			nums[index] = nums[replaceIndex]
 			replaceIndex--
 		} else {
 			index++
-			counter++
 		}
 	}
-	return counter
+	return index
 }
