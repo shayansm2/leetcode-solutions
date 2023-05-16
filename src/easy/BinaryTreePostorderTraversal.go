773ddd2c6b57bebf639d74cc50c1066ee469a4ae
shayansm <shayansm09@gmail.com>
2023-05-16 23:46:20 +0330

    resolved an easy problem

---
+++ src/easy/BinaryTreePostorderTraversal.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	. "../lib"
+	"fmt"
+)
+
+func postorderTraversal(root *TreeNode) []int {
+	var visits []int
+	postorderTraversalRecursively(root, &visits)
+	return visits
+}
+
+func postorderTraversalRecursively(node *TreeNode, visits *[]int) {
+	if node == nil {
+		return
+	}
+
+	postorderTraversalRecursively(node.Left, visits)
+	postorderTraversalRecursively(node.Right, visits)
+	visit(visits, node)
+}
+
+func visit(visits *[]int, node *TreeNode) {
+	*visits = append(*visits, node.Val)
+}
+
+func main() {
+	fmt.Println(postorderTraversal(&TreeNode{Val: 1, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}}))
+	fmt.Println(postorderTraversal(nil))
+	fmt.Println(postorderTraversal(&TreeNode{Val: 1}))
+}
