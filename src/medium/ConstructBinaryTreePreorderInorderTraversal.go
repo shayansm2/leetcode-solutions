f6e6ed6af075d333a64c1dbeb29c731198af5e9f
shayansm <shayansm09@gmail.com>
2023-05-17 11:39:02 +0330

    solved one of famous 150 leetcode questions

---
+++ src/medium/ConstructBinaryTreePreorderInorderTraversal.go
@@ -0,0 +1,24 @@
+package main
+
+import . "../lib"
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	rootIndex := 0
+	for inorder[rootIndex] != preorder[0] {
+		rootIndex++
+	}
+
+	leftPreOrder := preorder[1 : rootIndex+1]
+	rightPreOrder := preorder[rootIndex+1:]
+	leftInOrder := inorder[:rootIndex]
+	rightInOrder := inorder[rootIndex+1:]
+
+	left := buildTree(leftPreOrder, leftInOrder)
+	right := buildTree(rightPreOrder, rightInOrder)
+
+	return &TreeNode{Val: preorder[0], Left: left, Right: right}
+}
