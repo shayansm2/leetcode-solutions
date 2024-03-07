3bd31f9725f709676bfbf7cbd1ae7e046574725c
shayansm2 <shayansm09@gmail.com>
2024-03-07 16:25:36 +0330

    solved problem of the day

---
+++ src/easy/MiddleLinkedList.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	end := head
+	middle := head
+
+	for end != nil {
+		end = end.Next
+		if end == nil {
+			break
+		}
+		end = end.Next
+		middle = middle.Next
+	}
+
+	return middle
+}
+
+func main() {
+	fmt.Println(middleNode(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}))
+	fmt.Println(middleNode(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, &ListNode{6, nil}}}}}}))
+}
