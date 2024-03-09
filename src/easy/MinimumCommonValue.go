10c58f4b4f02e6f9dc345e4a36bb6012c7526c9f
shayansm2 <shayansm09@gmail.com>
2024-03-09 08:24:07 +0330

    solved problme of the day

---
+++ src/easy/MinimumCommonValue.go
@@ -0,0 +1,23 @@
+package main
+
+func getCommon(nums1 []int, nums2 []int) int {
+	i, j := 0, 0
+	for i < len(nums1) && j < len(nums2) {
+
+		if nums1[i] == nums2[j] {
+			return nums1[i]
+		}
+
+		if nums1[i] < nums2[j] {
+			i++
+			continue
+		}
+
+		if nums1[i] > nums2[j] {
+			j++
+			continue
+		}
+	}
+
+	return -1
+}
