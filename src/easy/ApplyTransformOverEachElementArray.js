32b7ed28313a52c54b9b365de2be3432c051696f
Shayan ShafieeMoghadam <shayansm09@gmail.com>
2024-12-26 17:28:42 +0330

    start learning js and solving problems

---
+++ src/easy/ApplyTransformOverEachElementArray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+  let result = [];
+  arr.forEach((element, index) => {
+    result.push(fn(element, index));
+  });
+  return result;
+};
+
+console.log(map([1,2,3], n=>n+1));
+console.log(map([1,2,3], (n,i)=>n+i));
+console.log(map([1,2,3], function constant() { return 42; }));
